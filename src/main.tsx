41871519b71af86d81fe2884c1753664892de311
RuannDutra <rdutrasantos12@gmail.com>
2026-04-21 14:32:17 -0300

    Att

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Remove Lovable badge if injected
+const removeLovableBadge = () => {
+  const badge = document.querySelector('[id^="lovable-badge"]');
+  if (badge) badge.remove();
+};
+const observer = new MutationObserver(removeLovableBadge);
+observer.observe(document.body, { childList: true, subtree: true });
+removeLovableBadge();
+
 createRoot(document.getElementById("root")!).render(<App />);
