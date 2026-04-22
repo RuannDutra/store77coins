3d684d1b1fd35bfb53164b3e77a690dc0fe1e408
RuannDutra <rdutrasantos12@gmail.com>
2026-04-22 13:13:04 -0300

    Teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Ponto de entrada da aplicação Store77Coins
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
